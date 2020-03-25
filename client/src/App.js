abd112120a91b7a4d8654241f17027956dcb0197
fbuitron <franciscobuitron21@gmail.com>
2020-03-25 14:28:11 -0700

    built out cards for projects for homepage view

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import HomePageCard from './components/HomePageCard';
 
 function App() {
- 
-}
+  return (
+    <>
+      <HomePageCard />
+      <HomePageCard />
+      <HomePageCard />
+      <HomePageCard />
+      <HomePageCard />
+    </>
+  )
+};
 
 export default App;
