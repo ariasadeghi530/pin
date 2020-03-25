9afbb303b45c5797f203836284c10a81ab7d3713
fbuitron <franciscobuitron21@gmail.com>
2020-03-25 13:22:50 -0700

    added .env ignore

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ passport.use(new JWTStrategy({
 app.use(require('./routes'))
 
 require('./config')
-  .then(() => app.listen(process.env.PORT || 3000))
+  .then(() => app.listen(process.env.PORT || 3001))
   .catch(e => console.log(e));
