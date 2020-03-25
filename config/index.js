d1ea929f6bf90a700cd89d06bbfa14928883861c
Aria <ariasadeghi@gmail.com>
2020-03-25 13:19:49 -0700

    updated local uir in env

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/pin',
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || process.env.LOCAL_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
