e6a4f2f82b8c36f2414b5a1090ba44ccc1f8cc8d
Aria <ariasadeghi@gmail.com>
2020-03-26 11:07:39 -0700

    added user routes for update and delete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,19 +6,21 @@ const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
-
+// User login
 router.post('/users/login', (req, res) => {
   User.authenticate()(req.body.username, req.body.password, (err, user) => {
     if (err) throw err;
     res.json({
       isLoggedIn: !!user,
-      posts: user.posts,
+      ideas: user.ideas,
+      projects: user.projects,
       user: user.username,
       token: jwt.sign({ id: user._id }, process.env.SECRET)
     });
   })
 })
 
+// User registration
 router.post('/users/register', (req, res) => {
   User.register(new User({
     username: req.body.username,
@@ -30,10 +32,28 @@ router.post('/users/register', (req, res) => {
   })
 })
 
-router.put('/users/:itemname', passport.authenticate('jwt'), (req, res) => {
-  User.findByIdAndUpdate(req.user._id, { $push: { items: req.params.itemname } })
-    .then((response) => res.json(response))
-    .catch(e => console.log(e));
-});
+//logout
+// router.get('/users/logout', passport.authenticate('jwt'),(req, res) => {
+  
+// })
+
+//update user info
+router.put('/users/update', passport.authenticate('jwt'), (req, res) => {
+  User.findByIdAndUpdate(req.user._id, req.body)
+  .then(user => res.json(user))
+  .catch(e => console.error(e));
+})
+
+// Pinning a project, saving a project idea
+router.put('/users/:projectID', passport.authenticate('jwt'), (req, res) => {
+  User.findByIdAndUpdate(req.user._id, { $push: { projects: req.params.projects}})
+})
+
+//Delete user account
+router.delete('/users/delete', passport.authenticate('jwt'), (req, res) => {
+  User.findByIdAndDelete(req.user._id)
+  .then(() => res.sendStatus(200))
+  .catch(e => console.error(e))
+})
 
 module.exports = router;
