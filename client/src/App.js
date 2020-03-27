5238e82c21f97ed8072edc9a253d5272e2053550
fbuitron <franciscobuitron21@gmail.com>
2020-03-27 12:37:47 -0700

    added reset password view with links

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SignIn from './components/views/SignIn';
 import HomePage from './components/views/HomePage';
 import SignUp from './components/views/SignUp';
+import Reset from './components/views/Reset';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 function App() {
@@ -16,6 +17,9 @@ function App() {
         </Route>
         <Route exact path="/">
           <HomePage />
+        </Route>
+        <Route exact path="/reset">
+          <Reset />
         </Route>
       </Switch>
     </Router>
--- client/src/components/views/Reset/Reset.js
+++ client/src/components/views/Reset/Reset.js
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -57,7 +55,10 @@ export default function SignIn() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          Reset your password
+        </Typography>
+        <Typography variant="subtitle2" color="textSecondary">
+          We will send a password reset link to your email
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
@@ -65,25 +66,10 @@ export default function SignIn() {
             margin="normal"
             required
             fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
+            name="email"
+            label="Email address"
+            type="email"
+            id="email"
           />
           <Button
             type="submit"
@@ -92,19 +78,12 @@ export default function SignIn() {
             color="primary"
             className={classes.submit}
           >
-            Sign In
+            Submit
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="/reset" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
+          <Grid item>
+            <Link href="/signin" variant="body2">
+              {"Already have an account? Sign In"}
+            </Link>
           </Grid>
         </form>
       </div>
