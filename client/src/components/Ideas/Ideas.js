c210be6fa52f63f4bcb3141ab57ade651f264e80
Evan Yang <evanyang@pop-os.localdomain>
2020-03-27 11:08:20 -0700

    ideas-page pull routes

---
+++ client/src/components/Ideas/Ideas.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Idea = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Project Idea Title</h1>
+            <h2>Author: Author</h2>
+
+            <ul>
+                <li>Comment #1</li>
+                <li>Comment #2</li>
+            </ul>
+
+        </div>
+        
+    )
+}
