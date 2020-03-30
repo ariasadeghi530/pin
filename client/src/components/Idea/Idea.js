2c3150c2b2e774da54ec189e13a5c950c6b95763
Evan Yang <evanyang@pop-os.localdomain>
2020-03-30 14:25:38 -0700

    Idea.js: card: title, author

--- client/src/components/Idea/Idea.js
+++ client/src/components/Idea/Idea.js
@@ -3,6 +3,11 @@ import Navbar from '../Navbar'
 import TextareaAutosize from '@material-ui/core/TextareaAutosize'
 import Button from '@material-ui/core/Button'
 import Icon from '@material-ui/core/Icon'
+
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
 //import axios from 'axios'
 
 import './Idea.css'
@@ -26,8 +31,20 @@ export default function Idea() {
     return (
         <div>
             <Navbar />
-            <h1>Project Idea Title</h1>
-            <h2>Author: Author</h2>
+            <br /> <br /> <br /> <br /> <br /> <br />
+            <Card>
+                <CardContent>
+                    <Typography color="textSecondary" gutterBottom>
+                    Author
+                    </Typography>
+                    <Typography variant="h5" component="h2">
+                    Title
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    
+                </CardActions>
+            </Card>
 
             <section className="section-solution">
                 <h3>Solution:</h3>
