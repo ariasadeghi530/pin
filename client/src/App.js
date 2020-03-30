8b7453a303b08857fe86b722c5062ee7a1c571a6
EvanErickson <evanserickson@gmail.com>
2020-03-30 11:19:06 -0700

    made changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar'
 import SignIn from './components/views/SignIn';
 import HomePage from './components/views/HomePage';
 import SignUp from './components/views/SignUp';
 import Reset from './components/views/Reset';
 import PrimarySearchAppBar from './components/Navbar'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+// import validators from './common/validators';
+// import Routes from './Routes';
+
+// const browserHistory = createBrowserHistory();
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <ThemeProvider theme={theme}>
+//         <Router history={browserHistory}>
+//           <Routes />
+//         </Router>
+//       </ThemeProvider>
+//     );
+//   }
+// }
+
 
 function App() {
   return (
+    <>
+    <Navbar />
     <Router>
       <Switch>
         <Route exact path="/signin">
           <SignIn />
         </Route>
         <Route exact path="/signup">
-          <PrimarySearchAppBar />
           <SignUp />
         </Route>
         <Route exact path="/">
+        <PrimarySearchAppBar />
           <HomePage />
         </Route>
         <Route exact path="/reset">
@@ -25,6 +45,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
+    </>
   )
 };
 
