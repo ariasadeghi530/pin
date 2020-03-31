3f1c5b9786ea4c7537eb76bee319c2e945347005
fbuitron <franciscobuitron21@gmail.com>
2020-03-31 13:36:14 -0700

    added new password view, removed link from footers

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="">
         PIN
       </Link>{' '}
       {new Date().getFullYear()}
--- client/src/components/views/Reset/Reset.js
+++ client/src/components/views/Reset/Reset.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -15,7 +13,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="">
         PIN
       </Link>{' '}
       {new Date().getFullYear()}
@@ -42,6 +40,11 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  signLogo: {
+    height: "20%",
+    width: "20%",
+    marginBottom: 7,
+  }
 }));
 
 export default function SignIn() {
@@ -51,9 +54,7 @@ export default function SignIn() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
+        <img src="https://image.flaticon.com/icons/svg/212/212816.svg" className={classes.signLogo} alt="pin logo" />
         <Typography component="h1" variant="h5">
           Reset your password
         </Typography>
