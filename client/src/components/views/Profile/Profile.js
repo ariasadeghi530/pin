abe45844dfa0c7e518a8c961475d807ebe8181c9
EvanErickson <evanserickson@gmail.com>
2020-03-31 12:50:56 -0700

    made changes

--- client/src/components/views/Profile/Profile.js
+++ client/src/components/views/Profile/Profile.js
@@ -6,6 +6,32 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Navbar from '../../Navbar'
+// import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+// import List from '@material-ui/core/List';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+// import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import Avatar from '@material-ui/core/Avatar';
+// import IconButton from '@material-ui/core/IconButton';
+// import FormGroup from '@material-ui/core/FormGroup';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
+// import Grid from '@material-ui/core/Grid';
+// import FolderIcon from '@material-ui/icons/Folder';
+// import DeleteIcon from '@material-ui/icons/Delete';
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+
+
+
+
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,7 +46,22 @@ const useStyles = makeStyles((theme) => ({
     fontSize: theme.typography.pxToRem(15),
     color: theme.palette.text.secondary,
   },
+  title: {
+    fontSize: 20,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  blah: {
+      marginTop: 10,
+    //   float: 'right'
+  },
+  floatRight: {
+    //   float: 'right'
+  }
 }));
+
+
 
 export default function ControlledExpansionPanels() {
   const classes = useStyles();
@@ -32,16 +73,47 @@ export default function ControlledExpansionPanels() {
 
   return (
       <>
-      {/* <Navbar /> */}
-    <div className={classes.root}>
+    <div className={classes.blah}>
+    <Card className={classes.root}>
+      <CardContent>
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <div className={classes.floatRight}>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Evan Erickson
+        </Typography>
+        <Typography variant="h5" component="h2">
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Github: EvanErickson
+        </Typography>
+        <Typography variant="body2" component="p">
+          MERN Stack, Vanilla Javascript, Python, and PHP.
+        </Typography>
+        </div>
+      </CardContent>
+      {/* <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions> */}
+    </Card>
+    </div>
+
+
+
+
+
+
+
+
+
+    <div className={classes.root, classes.blah}>
       <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <ExpansionPanelSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-          <Typography className={classes.heading}>General settings</Typography>
-          <Typography className={classes.secondaryHeading}>Change Email, Password, and Contact Information</Typography>
+          <Typography className={classes.heading}>Ideas</Typography>
+          <Typography className={classes.secondaryHeading}>View all ideas.</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
@@ -55,7 +127,7 @@ export default function ControlledExpansionPanels() {
           aria-controls="panel2bh-content"
           id="panel2bh-header"
         >
-          <Typography className={classes.heading}>Posts</Typography>
+          <Typography className={classes.heading}>Projects</Typography>
           <Typography className={classes.secondaryHeading}>
             Review all your posts (insert functionality and ability to edit / delete posts here)
           </Typography>
@@ -73,9 +145,9 @@ export default function ControlledExpansionPanels() {
           aria-controls="panel3bh-content"
           id="panel3bh-header"
         >
-          <Typography className={classes.heading}>Help</Typography>
+          <Typography className={classes.heading}>Solutions</Typography>
           <Typography className={classes.secondaryHeading}>
-            Click here to send an email to our admin.
+            Click here to see your solutions.
           </Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
@@ -91,11 +163,11 @@ export default function ControlledExpansionPanels() {
           aria-controls="panel4bh-content"
           id="panel4bh-header"
         >
-          <Typography className={classes.heading}>Personal data</Typography>
+          <Typography className={classes.heading}>Comments</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
-            To review our data policy and opt out of data collection, click here.
+            Insert comment table here
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
@@ -103,3 +175,143 @@ export default function ControlledExpansionPanels() {
     </>
   );
 }
+
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//     maxWidth: 752,
+//   },
+//   demo: {
+//     backgroundColor: theme.palette.background.paper,
+//   },
+//   title: {
+//     margin: theme.spacing(4, 0, 2),
+//   },
+// }));
+
+// function generate(element) {
+//   return [0, 1, 2].map((value) =>
+//     React.cloneElement(element, {
+//       key: value,
+//     }),
+//   );
+// }
+
+// export default function InteractiveList() {
+//   const classes = useStyles();
+//   const [dense, setDense] = React.useState(false);
+//   const [secondary, setSecondary] = React.useState(false);
+
+//   return (
+//     <div className={classes.root}>
+//       <FormGroup row>
+//         <FormControlLabel
+//           control={
+//             <Checkbox checked={dense} onChange={(event) => setDense(event.target.checked)} />
+//           }
+//           label="Enable dense"
+//         />
+//         <FormControlLabel
+//           control={
+//             <Checkbox
+//               checked={secondary}
+//               onChange={(event) => setSecondary(event.target.checked)}
+//             />
+//           }
+//           label="Enable secondary text"
+//         />
+//       </FormGroup>
+//       <Grid container spacing={2}>
+//         <Grid item xs={12} md={6}>
+//           <Typography variant="h6" className={classes.title}>
+//             Text only
+//           </Typography>
+//           <div className={classes.demo}>
+//             <List dense={dense}>
+//               {generate(
+//                 <ListItem>
+//                   <ListItemText
+//                     primary="Single-line item"
+//                     secondary={secondary ? 'Secondary text' : null}
+//                   />
+//                 </ListItem>,
+//               )}
+//             </List>
+//           </div>
+//         </Grid>
+//         <Grid item xs={12} md={6}>
+//           <Typography variant="h6" className={classes.title}>
+//             Icon with text
+//           </Typography>
+//           <div className={classes.demo}>
+//             <List dense={dense}>
+//               {generate(
+//                 <ListItem>
+//                   <ListItemIcon>
+//                     <FolderIcon />
+//                   </ListItemIcon>
+//                   <ListItemText
+//                     primary="Single-line item"
+//                     secondary={secondary ? 'Secondary text' : null}
+//                   />
+//                 </ListItem>,
+//               )}
+//             </List>
+//           </div>
+//         </Grid>
+//       </Grid>
+//       <Grid container spacing={2}>
+//         <Grid item xs={12} md={6}>
+//           <Typography variant="h6" className={classes.title}>
+//             Avatar with text
+//           </Typography>
+//           <div className={classes.demo}>
+//             <List dense={dense}>
+//               {generate(
+//                 <ListItem>
+//                   <ListItemAvatar>
+//                     <Avatar>
+//                       <FolderIcon />
+//                     </Avatar>
+//                   </ListItemAvatar>
+//                   <ListItemText
+//                     primary="Single-line item"
+//                     secondary={secondary ? 'Secondary text' : null}
+//                   />
+//                 </ListItem>,
+//               )}
+//             </List>
+//           </div>
+//         </Grid>
+//         <Grid item xs={12} md={6}>
+//           <Typography variant="h6" className={classes.title}>
+//             Avatar with text and icon
+//           </Typography>
+//           <div className={classes.demo}>
+//             <List dense={dense}>
+//               {generate(
+//                 <ListItem>
+//                   <ListItemAvatar>
+//                     <Avatar>
+//                       <FolderIcon />
+//                     </Avatar>
+//                   </ListItemAvatar>
+//                   <ListItemText
+//                     primary="Single-line item"
+//                     secondary={secondary ? 'Secondary text' : null}
+//                   />
+//                   <ListItemSecondaryAction>
+//                     <IconButton edge="end" aria-label="delete">
+//                       <DeleteIcon />
+//                     </IconButton>
+//                   </ListItemSecondaryAction>
+//                 </ListItem>,
+//               )}
+//             </List>
+//           </div>
+//         </Grid>
+//       </Grid>
+//     </div>
+//   );
+// }
